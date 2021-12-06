2d32c05549e14d09c73b9bf6a6ac608ae0bf439d
HubashovD <34233868+HubashovD@users.noreply.github.com>
2021-12-06 21:10:20 +0200

    rev_3

--- scripts/map.js
+++ scripts/map.js
@@ -1,84 +1,61 @@
-/**
- * Created by yevheniia on 09.06.20.
- */
- var default_zoom_u = window.innerWidth > 800 ? 14 : 24;
- var default_zoom_k = window.innerWidth > 800 ? 24 : 1;
+var default_zoom_u = window.innerWidth > 800 ? 5 : 5;
+var default_zoom_k = window.innerWidth > 800 ? 9 : 8; 
+  
+mapboxgl.accessToken = 'pk.eyJ1IjoiZHJpbWFjdXMxODIiLCJhIjoiWGQ5TFJuayJ9.6sQHpjf_UDLXtEsz8MnjXw';
  
- var stops_values = [
-     ["Приватна власність", 'white'],
-     [-1, '#d3d3d3'],
-     [0, '#ffffff'],
-     [1, '#ffffb2'],
-     [2, '#fed976'],
-     [3, "#feb24c"],
-     [6, "#fd8d3c"],
-     [8, "#f03b20"],
-     [10, "#bd0026"]
- ];
- 
- mapboxgl.accessToken = 'pk.eyJ1IjoiZHJpbWFjdXMxODIiLCJhIjoiWGQ5TFJuayJ9.6sQHpjf_UDLXtEsz8MnjXw';
- 
- var map = new mapboxgl.Map({
+var map = new mapboxgl.Map({
      container: 'map',
      minZoom: default_zoom_u,
-     maxZoom: default_zoom_u + 2,
+     maxZoom: 20,
      hash: false,
      tap: false,
-     attributionControl: true,
+     attributionControl: false,
      style: 'mapbox://styles/mapbox/dark-v10',
-     center: [30.5, 50.4],
+     center: [31.5, 48.5],
      zoom: default_zoom_u // starting zoom
  });
  
- 
+map.scrollZoom.disable();  
 
+map.on('load', function () {
+    var layers = map.getStyle().layers;
+    var firstSymbolId;
+        
+    for (var i = 0; i < layers.length; i++) {
+            if (layers[i].type === 'symbol') {
+                firstSymbolId = layers[i].id;
+                break;
+            }
+        }
  
- 
- Promise.all([
-     d3.csv("https://raw.githubusercontent.com/texty/covid_schools_map/master/data/TABLE.csv"),
-     d3.csv("https://raw.githubusercontent.com/texty/covid_schools_map/master/data/smallTable.csv")
- ]).then(function(data) {
- 
-     data[0].forEach(function(d){
-         d.pot_infections = +d.pot_infections;
+    //векторні тайли
+         map.addSource('schools', {
+             type: 'vector',
+             tiles: ["https://HubashovD.github.io/kyiv_kadastr_web/vector_tiles/parcels/{z}/{x}/{y}.pbf"]
      });
  
-     var layers = map.getStyle().layers;
-     var firstSymbolId;
  
-     for (var i = 0; i < layers.length; i++) {
-         if (layers[i].type === 'symbol') {
-             firstSymbolId = layers[i].id;
-             break;
-         }
-     }
- 
- 
-     /* ------- карта України ------- */
-     map.on('load', function () {
- 
-         //векторні тайли
-         map.addSource('parcels_4326', {
-             type: 'vector',
-             tiles: ["https://hubashovd.github.io/kyiv_kadastr_web/tree/main/vector_tiles/parcels/{z}/{x}/{y}.pbf"] 
-         });
- 
- 
-         function redrawUkraineMap(choropleth_column) {
+     function redrawUkraineMap() {
              map.addLayer({
-                 "id": "parcels_4326",
+                 "id": "schools_data",
                  'type': 'fill',
                  'minzoom': 4,
-                 'maxzoom': 10,
-                 'source': "parcels_4326",
+                 'maxzoom': 15,
+                 'source': "schools",               
                  "source-layer": "parcels_4326",
-                 "paint": {
-                     'fill-color': {
-                         property: choropleth_column,
-                         stops: stops_values
-                     },
- 
- 
+                 "paint": {   
+                    'fill-color': ['match',
+                    ['get', 'ownership'],
+                    "Приватна власність",
+                    'green',
+                    "Комунальна власність",
+                    'red',
+                    "Державна власність",
+                    'blue',
+                    "Не визначено",
+                    '#FFFFB2',
+                    /* other */ '#ccc'
+                  ],
                      'fill-outline-color': [
                          'case',
                          ['boolean', ['feature-state', 'hover'], false],
@@ -89,7 +66,7 @@
              }, firstSymbolId);
          }
  
-         map.on('click', 'parcels_4326', function(e) {
+         map.on('click', 'schools_data', function(e) {
             map.getCanvas().style.cursor = 'pointer';
             new mapboxgl.Popup()
                  .setLngLat(e.lngLat)
@@ -97,21 +74,9 @@
                  .addTo(map);
          });
  
-         redrawUkraineMap('parcels_4326');
- 
-         /* перемикаємо шари  карти */
-         d3.select("#ukraine-switch-buttons").selectAll(".map_button").on("click", function() {
-             let selected_layer = d3.select(this).attr("value");
-             d3.select(this.parentNode).selectAll(".map_button").classed("active", false);
-             d3.select(this).classed("active", true);
-             map.removeLayer('parcels_4326');
-             redrawUkraineMap(selected_layer);
-         });
- 
+         redrawUkraineMap(); 
  
          var nav = new mapboxgl.NavigationControl();
          map.addControl(nav, 'top-left');
  
      }); //end of Ukraine map
-     
- });
